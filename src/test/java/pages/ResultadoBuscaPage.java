8a952b4720475b29173c1891c7073bc1e1834e53
Roni Lee <roni.jimmylee@gmail.com>
2021-07-03 10:34:56 -0300

    Atualização de codigo e exe chromedriver

--- src/test/java/pages/ResultadoBuscaPage.java
+++ src/test/java/pages/ResultadoBuscaPage.java
@@ -8,7 +8,7 @@ public class ResultadoBuscaPage {
 
 	WebDriver driver;
 
-	final String PRODUTO_BUSCA = "//span[contains(text(),'iPhone 11 Pro Max 64 GB Tela 6.5 Polegadas')]";
+	final String PRODUTO_BUSCA = "//body/div[@id='a-page']/div[@id='search']/div[1]/div[1]/div[1]/div[1]/span[3]/div[2]/div[1]/div[1]/span[1]/div[1]/div[1]/span[1]/a[1]/div[1]/img[1]";
 	final String PRECO_PRODUTO = "//span[@id='price_inside_buybox']";
 
 	public ResultadoBuscaPage(WebDriver driver) {
--- src/test/resources/feature/exemploCucumber.feature
+++ src/test/resources/feature/exemploCucumber.feature
@@ -2,5 +2,7 @@
 Funcionalidade: Testes exemplo Cucumber
 Cenário: Teste cucumber Amazon
 Dado que esteja na pagina: "http://www.amazon.com.br"
-Quando fizer a busca pelo produto: "Iphone 11 pro max" 
-Então vou verificar se o valor está acima de "R$9.125,99"
+Quando fizer a busca pelo produto: "Iphone 11 Apple Preto, 128gb" 
+Então vou verificar se o valor está acima de "R$4.752,10"
+
+
